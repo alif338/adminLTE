aae38fcb8d61c1f02e42b82b0dad428837d856c1
MalifD <59762698+alif338@users.noreply.github.com>
2020-08-06 11:52:56 +0700

    Add files via upload

--- resources/views/items/data_table.blade.php
+++ resources/views/items/data_table.blade.php
@@ -4,6 +4,12 @@
        @include('items.tables')
 @endsection
 
+@push('style')
+<link rel="stylesheet" href="{{ asset('/admin_Lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/admin_Lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+@endpush
+
+
 @push('scripts')
 <script src="{{ asset('/admin_Lte/plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('/admin_Lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
